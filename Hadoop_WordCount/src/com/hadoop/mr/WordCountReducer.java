ad765f5b3e438c44000abe1421c33df30622cafe
varunsharma5 <varunge@gmail.com>
2014-12-17 12:10:26 +0530

    One more test change

--- Hadoop_WordCount/src/com/hadoop/mr/WordCountReducer.java
+++ Hadoop_WordCount/src/com/hadoop/mr/WordCountReducer.java
@@ -4,6 +4,11 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+/**
+ * 
+ * @author varun
+ *
+ */
 public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
 	protected void reduce(Text key, java.lang.Iterable<IntWritable> values, Context context) throws java.io.IOException ,InterruptedException {
 		//Defining a local variable sum of type int
